03b0c1ab40aa87ef14fa52302262570ac1793a86
ed-matheus <edsonmatheus02@hotmail.com>
2025-09-19 23:20:18 -0300

    pequeno ajuste no Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="w-full mb-5 p-8 flex flex-col md:flex-row md:px-20 lg:px-30 xl:px-45 gap-3 justify-between items-center">
+    <header className="w-full p-8 flex flex-col md:flex-row md:px-20 lg:px-30 xl:px-45 gap-3 justify-between items-center">
       <h2 className="font-[500] text-xl text-cyan mb-2">Matheus Costa</h2>
 
       <ul className="flex gap-8 font-[500]">
